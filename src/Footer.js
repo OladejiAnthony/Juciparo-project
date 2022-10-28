01ae847e66cfab5fe291785bf63e4be9bb33555a
Oladeji Anthony <oladejianthony98@gmail.com>
2022-10-28 06:28:28 +0100

    footer and newsletter section

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,27 @@ function Footer() {
   return (
     <div className='footer__container'>
       <img src={logo} alt='logo' />
+      <div className='footer__list'>
+        <ul>
+          <li>Help</li>
+          <li>Privacy Policy</li>
+          <li>FAQs</li>
+          <li>Track My Order</li>
+          <li>Contact Us</li>
+        </ul>
+        <ul>
+          <li>Store</li>
+          <li>Top Products</li>
+          <li>Official Store</li>
+          <li>Become a seller</li>
+        </ul>
+        <ul>
+          <li>Support</li>
+          <li>About Us</li>
+          <li>Customer service</li>
+          <li>Terms and Condition</li>
+        </ul>
+      </div>
         
     </div>
   )
