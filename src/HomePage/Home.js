5aa538d24b842edbc413d0180dd98b9bc37bab19
Oladeji Anthony <oladejianthony98@gmail.com>
2022-10-29 07:07:19 +0100

    landing page first carousel row

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,14 +1,61 @@
 import React from 'react'
 import "./Home.css"
 import Landing from './Landing'
-import Next from './Next'
+import FirstCarousel from './FirstCarousel'
+
+import img1 from "../Images/pngwing.jpg"
+import img2 from "../Images/pngwing1.jpg"
+import img3 from "../Images/pngwing2.jpg"
+import img4 from "../Images/pngwing3.jpg"
+
 
 function Home() {
   return (
     <>
       <div className='home__container'>
         <Landing />
-        <Next />
+        <FirstCarousel 
+          show={3} 
+          style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}
+        >
+          <div>
+            <div className="img">
+              <img src={img1} alt="placeholder"  /> 
+            </div>
+            <p>Fashion</p>
+          </div>
+          <div>
+            <div className="img">
+              <img src={img2} alt="placeholder"  /> 
+            </div>
+            <p>Computers</p>
+          </div>
+          <div>
+            <div className="img">
+              <img src={img3} alt="placeholder"  /> 
+            </div> 
+            <p>Home & Office</p>
+          </div>
+          <div>
+            <div className="img">
+              <img src={img4} alt="placeholder"  /> 
+            </div>
+            <p>Phone & Tablet</p>
+          </div>
+          <div>
+            <div className="img">
+              <img src={img1} alt="placeholder"  /> 
+            </div>
+            <p>Others</p>
+          </div>
+          <div>
+            <div className="img">
+              <img src={img2} alt="placeholder"  /> 
+            </div>
+            <p>Electronics</p>
+          </div>
+          
+        </FirstCarousel>
       </div>
     </>
   )
--- src/HomePage/Landing.js
+++ src/HomePage/Landing.js
@@ -2,12 +2,17 @@ import React from 'react'
 import "./Landing.css"
 import Sidebar from './Sidebar'
 import bcg from "../Images/bcg.jpg"
+import Slideshow from './SlideShow'
 
 function Landing() {
   return (
     <div className='landing'>
       <Sidebar />
       <div className='landing__right'>
+        <div className='landing__slideShow'>
+          <Slideshow />
+        </div>
+
         <img src={bcg} alt='background' />
         <div className='landing__text'>
             kshdjks
