efd983cca4a34d6f585af5d4849e9411f531662f
Oladeji Anthony <oladejianthony98@gmail.com>
2022-11-28 09:08:46 +0100

    login pages responsiveness

--- src/LoginPage/AccountHeader.js
+++ src/LoginPage/AccountHeader.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function AccountHeader(props) {
   return (
     <div className='account_head'>
-        <img src={props.img} alt='logo' />
+        <Link to='/'>
+          <img src={props.img} alt='logo' />
+        </Link>
         <h3>{props.head}</h3>
         <h5>{props.headFive}</h5>
     </div>
--- src/LoginPage/CreateHead.js
+++ src/LoginPage/CreateHead.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function CreateHead(props) {
   return (
     <div className='create__head'>
-        <img src={props.img} alt='logo' />
+        <Link to='/'>
+          <img src={props.img} alt='logo' />
+        </Link>
         <h3>{props.head}</h3>
         <h5>{props.headFive}</h5>
     </div>
