d003eaad59150b985ba8fbb06d1396ab34e82f99
Oladeji Anthony <oladejianthony98@gmail.com>
2022-12-05 14:31:08 +0100

    terms and condition page

---
+++ src/Help/TermsCardTwo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TermsCardTwo(props) {
+  return (
+    <div className='terms--card--two'>
+      <h4>{props.head}</h4>
+      <p>{props.text}</p>
+    </div>
+  )
+}
+
+export default TermsCardTwo
--- src/Help/TermsUse.js
+++ src/Help/TermsUse.js
@@ -3,7 +3,42 @@ import React from 'react'
 function TermsUse() {
   return (
     <div className='termsUse'>
-      Terms Use
+      <h4>Use of Juciparo Website</h4>
+      <h5>You shall not host, display, upload, modify, publish, transmit, update or share any information that:</h5>
+      <ol>
+        <li>belongs to another person and to which You do not have any right.</li>
+        <li>is grossly harmful, harassing, blasphemous, defamatory, obscene, pornographic, paedophilic, libellous, invasive of another's privacy, hateful, or racially, ethnically objectionable, disparaging, relating or encouraging money laundering or gambling, or otherwise unlawful in any manner whatever; or unlawfully threatening or unlawfully harassing including but not limited to "indecent representation of women" or misleading in any way.</li>
+        <li>is patently offensive to the online community, such as sexually explicit content, or content that promotes obscenity, pedophilia, racism, bigotry, hatred or physical harm of any kind against any group or individual and also to harass or advocate harassment of another person;</li>
+        <li>involves the transmission of "junk mail," "chain letters," or unsolicited mass mailing or "spamming";</li>
+        <li>promotes illegal activities or conduct that is abusive, threatening, obscene, defamatory or libelous;</li>
+        <li>infringes upon or violates any third party's rights [(including, but not limited to, intellectual property rights, rights of privacy (including without limitation unauthorized disclosure of a person's name, email address, physical address or phone number) or rights of publicity];</li>
+        <li>promotes an illegal or unauthorized copy of another person's copyrighted work (see "Copyright complaint" below for instructions on how to lodge a complaint about uploaded copyrighted material), such as providing pirated computer programs or links to them, providing information to circumvent manufacture-installed copy-protect devices, or providing pirated music or links to pirated music files;</li>
+        <li>contains restricted or password-only access pages, or hidden pages or images (those not linked to or from another accessible page);</li>
+        <li>Provides material that exploits people in a sexual, violent or otherwise inappropriate tvttmanner or solicits personal information from anyone;</li>
+        <li>provides instructional information about illegal activities such as making or buying illegal weapons, violating someone's privacy, or providing or creating computer viruses;</li>
+        <li>contains video, photographs, or images of another person age 18 or older without his or her express written consent and permission or those of any minor (regardless of whether you have consent from the minor or his or her legal guardian).</li>
+        <li>tries to gain unauthorized access or exceeds the scope of authorized access (as defined herein and in other applicable Codes of Conduct or End User Access and License Agreements) to the Sites or to profiles, blogs, communities, account information, bulletins, friend request, or other areas of the Sites or solicits passwords or personal identifying information for commercial or unlawful purposes from other users;</li>
+        <li>engages in commercial activities and/or sales without Juciparo's prior written consent such as contests, sweepstakes, barter, advertising and pyramid schemes, or the buying or selling of "virtual" items related to the Sites. </li>
+        <li>Solicits gambling or engages in any gambling activity which Juciparo, in its sole discretion, believes is or could be construed as being illegal;</li>
+        <li>interferes with another user's use and enjoyment of Jumia Website or any other individual's user and enjoyment of similar services;</li>
+        <li>Refers to any website or URL that, in the sole discretion of Juciparo, contains material that is inappropriate for the Juciparo Website or any other Website, contains content that would be prohibited or violates the letter or spirit of these Terms of Use.</li>
+        <li>harm minors in any way; infringes any patent, trademark, copyright or other proprietary rights or third party’s trade secrets or rights of publicity or privacy or shall not be fraudulent or involve the sale of counterfeit or stolen items;</li>
+        <li>violates any law for the time being in force; deceives or misleads the addressee/ users about the origin of such messages or communicates any information which is grossly offensive or menacing in nature; impersonate another person;</li>
+        <li>Contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer resource; or contains any Trojan horses, worms, time bombs, cancelbots, easter eggs or other computer programming routines that may damage, detrimentally interfere with, diminish value of, surreptitiously intercept or expropriate any system, data or personal information;</li>
+        <li>Threatens the unity, integrity, defence, security or sovereignty of The Federal Republic of Nigeria, friendly relations with foreign states, or public order or causes incitement to the commission of any cognizable offence or prevents investigation of any offence or is insulting any other nation.
+Shall not be false, inaccurate or misleading;</li>
+        <li>Shall not, directly or indirectly,offer, attempt to offer;trade or attempt to trade in any item, the dealing of which is prohibited or restricted in any manner under the provisions of any applicable law, rule, regulation or guideline for the time being in force.</li>
+        <li>Shall not create liability for us or cause us to lose (in whole or in part) the services of our ISPs or other suppliers;</li>
+        <li>You shall not use any “deep-link”, “page-scrape”, “robot”, “spider” or other automatic device, program, algorithm or methodology, or any similar or equivalent manual process, to access, acquire, copy or monitor any portion of the Website or any Content, or in any way reproduce or circumvent the navigational structure or presentation of the Website or any Content, to obtain or attempt to obtain any materials, documents or information through any means not purposely made available through the Website. 
+</li>
+        <li>You shall not attempt to gain unauthorized access to any portion or feature of Juciparo website, or any other systems or networks connected to Konga.com Website or to any Konga server, computer, network, or to any of the services offered on or through Konga.com Website, by hacking, password “mining” or any other illegitimate means.</li>
+        <li>You shall not probe, scan or test the vulnerability of Juciparo website or any network connected to Juciparo website nor breach the security or authentication measures on Juciparo website or any network connected to the Website.</li>
+        <li>You agree that you will not take any action that imposes an unreasonable or disproportionately large load on the infrastructure of Juciparo Website or Juciparo’s systems or networks, or any systems or networks connected to Juciparo.</li>
+        <li>You agree not to use any device, software or routine to interfere or attempt to interfere with the proper working of the Website or any transaction being conducted on the Website, or with any other person’s use of the Website.;</li>
+        <li>You may not forget headers or otherwise manipulate identifiers in order to disguise the origin of any message or transmittal you send to Juciparo on or through the Website or any service offered on or through the Website. You may not pretend that you are, or that you represent, someone else, or impersonate any other individual or entity</li>
+        <li>You shall not exaggerate or over emphasize the attributes of such items or services so as to mislead other Users in any manner.</li>
+        <li>You shall not lift (i.e. copy and paste) images of products you want to sell on Juciparo from other websites, images to be used must be original images and not stolen images or content posted on another site. And in the event that you violate this term, you hereby indemnify Juciparo of any third party claim thereon.</li>
+      </ol>
     </div>
   )
 }
